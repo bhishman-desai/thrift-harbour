3435234b09ba6d1502c680104e25ca56802e1de4
kkulkarni <kr677398@dal.ca>
2024-02-07 18:41:49 -0400

    fix: unused imports

--- frontend/thrift-harbour/src/App.tsx
+++ frontend/thrift-harbour/src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Registration from "./features/registration/RegistrationScreen";
