66954be0ce854a72f66dd24c4ccded76955d64d5
Bhishman <bh352449@dal.ca>
2024-02-15 22:51:21 -0400

    fix: Changing redirect config and backend url

--- frontend/thrift-harbour/src/utils/Path.ts
+++ frontend/thrift-harbour/src/utils/Path.ts
@@ -8,7 +8,7 @@ export class Path {
     if (this.environment === "development") {
       return "http://localhost:8080/api/v1";
     } else {
-      return "http://172.17.1.50:8080/api/v1";
+      return "thrift/api/v1";
     }
   }
 
