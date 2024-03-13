9ca8512d29472a9765bcc24ab239edb849bd610f
kkulkarni <kr677398@dal.ca>
2024-03-12 21:30:50 -0300

    feat: static profile  page

--- frontend/thrift-harbour/src/features/user-profile/UserProfile.tsx
+++ frontend/thrift-harbour/src/features/user-profile/UserProfile.tsx
@@ -1,14 +1,89 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import ImageIcon from "../../assets/icons/ImageIcon";
 import Modal from "../../components/ui-components/Modal/Modal";
-import { Profile } from "./UserProfileStyles";
+import { Ratings } from "../buy-product/BuyProductsStyles";
+import { ProductImage } from "../product-listing/add-listing/ProductListingStyles";
+import {
+  Header,
+  InfoContainer,
+  Profile,
+  RatingContainer,
+  Title,
+  UserInfo,
+  Value,
+} from "./UserProfileStyles";
+import Stack from "@mui/material/Stack";
+import Rating from "@mui/material/Rating";
+import ProfileIcon from "../../assets/icons/ProfileIcon";
 
 const BuyProducts: React.FC = () => {
   const navigate = useNavigate();
+  const [thumbnailUrl, setThumbnailUrl] = useState<string | null>(null);
 
   return (
     <>
-      <h1>User profile</h1>
+      <Profile>
+        <label htmlFor="file-input">
+          {thumbnailUrl ? (
+            <ProductImage>
+              <img
+                style={{ borderRadius: "50%" }}
+                height={"100%"}
+                width={"100%"}
+                src={thumbnailUrl}
+                alt="Thumbnail"
+              />
+            </ProductImage>
+          ) : (
+            <ProductImage>
+              <ProfileIcon height={32} width={32} />
+            </ProductImage>
+          )}
+        </label>
+      </Profile>
+      <InfoContainer>
+        <UserInfo>
+          <Title>First Name:</Title>
+          <Value style={{ marginLeft: "4px" }}>Krutik</Value>
+        </UserInfo>
+        <UserInfo>
+          <Title>Last Name:</Title>
+          <Value style={{ marginLeft: "4px" }}>Kulkarni</Value>
+        </UserInfo>
+        <UserInfo>
+          <Title>Email:</Title>
+          <Value style={{ marginLeft: "4px" }}>
+            Kulkarnikrutik2000@gmail.com
+          </Value>
+        </UserInfo>
+        <RatingContainer>
+          <Title>Ratings as buyer:</Title>
+          <Value style={{ marginLeft: "4px" }}>
+            <Stack spacing={1}>
+              <Rating
+                name="half-rating-read"
+                defaultValue={4.5}
+                precision={0.5}
+                readOnly
+              />
+            </Stack>
+          </Value>
+        </RatingContainer>
+        <RatingContainer>
+          <Title>Ratings as seller:</Title>
+          <Value style={{ marginLeft: "4px" }}>
+            <Stack spacing={1}>
+              <Rating
+                name="half-rating-read"
+                defaultValue={4.5}
+                precision={0.5}
+                readOnly
+              />
+            </Stack>
+          </Value>
+        </RatingContainer>
+      </InfoContainer>
     </>
   );
 };
--- frontend/thrift-harbour/src/features/user-profile/UserProfileStyles.tsx
+++ frontend/thrift-harbour/src/features/user-profile/UserProfileStyles.tsx
@@ -1,5 +1,54 @@
 import styled from "styled-components";
 
+export const Header = styled.div`
+  line-height: 28px;
+  color: rgb(33, 33, 33);
+  font-family: Inter;
+  font-size: 20px;
+  font-weight: 600;
+`;
+
 export const Profile = styled.div`
-  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+// export const Profile = styled.div`
+//   display: flex;
+//   flex-direction: row;
+//   margin-top: 24px;
+// `;
+export const InfoContainer = styled.div`
+  width: 50%;
+`;
+
+export const UserInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 16px;
+`;
+
+export const Title = styled.div`
+  color: #212121;
+  font-family: inter;
+  font-size: 16px;
+  line-height: 20px;
+  font-weight: 600;
+`;
+
+export const Value = styled.div`
+  color: #616161;
+  font-family: inter;
+  line-height: 20px;
+  font-size: 16px;
+  font-weight: 400;
+`;
+
+export const RatingContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-top: 16px;
 `;
