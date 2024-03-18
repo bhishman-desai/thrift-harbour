8c282184fcc5a91a19597e58008ad89116f1b2b1
Mithun Khanna Baskaran <mt813453@dal.ca>
2024-03-17 21:31:47 -0300

    Add Bidding model and repository

--- backend/thrift-harbour/src/test/java/tech/group15/thriftharbour/service/implementation/BiddingServiceImplTest.java
+++ backend/thrift-harbour/src/test/java/tech/group15/thriftharbour/service/implementation/BiddingServiceImplTest.java
@@ -1,12 +1,24 @@
 package tech.group15.thriftharbour.service.implementation;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.Optional;
-
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;import tech.group15.thriftharbour.repository.AuctionSaleListingRepository;import tech.group15.thriftharbour.repository.BiddingRepository;import java.util.Optional;
+import static org.mockito.Mockito.*;
 class BiddingServiceImplTest {
-    BiddingServiceImpl biddingServiceImpl = new BiddingServiceImpl();
+    @Mock
+    AuctionSaleListingRepository auctionSaleListingRepository;
+    @Mock
+    BiddingRepository biddingRepository;
+    @InjectMocks
+    BiddingServiceImpl biddingServiceImpl;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
     void testPlaceBid(){
@@ -14,3 +26,5 @@ class BiddingServiceImplTest {
         Assertions.assertEquals(null, result);
     }
 }
+
+//Generated with love by TestMe :) Please raise issues & feature requests at: https://weirddev.com/forum#!/testme
