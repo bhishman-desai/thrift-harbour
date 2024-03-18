d59883880111bc9370bf7d9cd0c0dfe60c5682d3
Mithun Khanna Baskaran <mt813453@dal.ca>
2024-03-17 23:23:59 -0300

    Add Exception and tests

--- backend/thrift-harbour/src/main/java/tech/group15/thriftharbour/service/implementation/BiddingServiceImpl.java
+++ backend/thrift-harbour/src/main/java/tech/group15/thriftharbour/service/implementation/BiddingServiceImpl.java
@@ -1,10 +1,20 @@
 package tech.group15.thriftharbour.service.implementation;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import tech.group15.thriftharbour.constant.ErrorConstant;
+import tech.group15.thriftharbour.constant.InfoConstant;
+import tech.group15.thriftharbour.exception.ListingNotFoundException;
+import tech.group15.thriftharbour.exception.LowBidException;
+import tech.group15.thriftharbour.model.AuctionSaleListing;
+import tech.group15.thriftharbour.model.Bidding;
+import tech.group15.thriftharbour.model.User;
 import tech.group15.thriftharbour.repository.AuctionSaleListingRepository;
 import tech.group15.thriftharbour.repository.BiddingRepository;
+import tech.group15.thriftharbour.repository.UserRepository;
 import tech.group15.thriftharbour.service.BiddingService;
+import tech.group15.thriftharbour.service.JWTService;
 
 import java.util.Optional;
 
@@ -12,13 +22,46 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class BiddingServiceImpl implements BiddingService {
 
-    private final AuctionSaleListingRepository auctionSaleListingRepository;
+  private final JWTService jwtService;
 
-    private final BiddingRepository biddingRepository;
+  private final AuctionSaleListingRepository auctionSaleListingRepository;
 
-    @Override
-    public String placeBid(String authorizationHeader,String auctionSaleListingID, Double bidAmount) {
+  private final BiddingRepository biddingRepository;
 
-        return "";
+  private final UserRepository userRepository;
+
+  @Override
+  public String placeBid(String authorizationHeader, String auctionSaleListingID, Double bidAmount) {
+
+    String userEmail = jwtService.extractUserNameFromRequestHeaders(authorizationHeader);
+
+    User seller =
+        userRepository
+            .findByEmail(userEmail)
+            .orElseThrow(() -> new UsernameNotFoundException(ErrorConstant.USER_NOT_FOUND));
+
+    AuctionSaleListing auctionSaleListing = auctionSaleListingRepository.findByAuctionSaleListingID(auctionSaleListingID);
+
+    if(auctionSaleListing == null){
+      throw new ListingNotFoundException(ErrorConstant.PRODUCT_NOT_FOUND);
     }
+
+    if(bidAmount <= auctionSaleListing.getHighestBid()){
+      throw new LowBidException(ErrorConstant.LOW_BID_EXCEPTION);
+    }
+
+    Bidding userPlacedBid = Bidding.builder()
+            .auctionSaleListingID(auctionSaleListing.getAuctionSaleListingID())
+            .userEmail(userEmail)
+            .placedBid(bidAmount)
+            .build();
+
+    auctionSaleListing.setHighestBid(bidAmount);
+    auctionSaleListing.setCurrentHighestBidUserMail(seller.getEmail());
+
+    auctionSaleListingRepository.save(auctionSaleListing);
+    biddingRepository.save(userPlacedBid);
+
+    return InfoConstant.BID_PLACED_SUCCESFULLY;
+  }
 }
--- backend/thrift-harbour/src/test/java/tech/group15/thriftharbour/service/implementation/BiddingServiceImplTest.java
+++ backend/thrift-harbour/src/test/java/tech/group15/thriftharbour/service/implementation/BiddingServiceImplTest.java
@@ -6,9 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import tech.group15.thriftharbour.constant.InfoConstant;
 import tech.group15.thriftharbour.enums.RoleEnum;
+import tech.group15.thriftharbour.exception.ListingNotFoundException;
+import tech.group15.thriftharbour.exception.LowBidException;
 import tech.group15.thriftharbour.model.AuctionSaleListing;
+import tech.group15.thriftharbour.model.Bidding;
 import tech.group15.thriftharbour.model.User;
 import tech.group15.thriftharbour.repository.AuctionSaleListingRepository;
 import tech.group15.thriftharbour.repository.BiddingRepository;
@@ -20,35 +24,104 @@ import java.util.GregorianCalendar;
 import java.util.Optional;
 
 import static org.mockito.Mockito.*;
+
 class BiddingServiceImplTest {
-    @Mock
-    AuctionSaleListingRepository auctionSaleListingRepository;
-    @Mock
-    BiddingRepository biddingRepository;
+  @Mock AuctionSaleListingRepository auctionSaleListingRepository;
+  @Mock BiddingRepository biddingRepository;
 
-    @Mock
-    UserRepository userRepository;
+  @Mock UserRepository userRepository;
 
-    @Mock
-    JWTService jwtService;
+  @Mock JWTService jwtService;
 
-    @InjectMocks
-    BiddingServiceImpl biddingServiceImpl;
+  @InjectMocks BiddingServiceImpl biddingServiceImpl;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
+  AuctionSaleListing auctionProduct;
 
-    @Test
-    void testPlaceBid(){
+  Bidding userBid;
 
-        when(auctionSaleListingRepository.findByAuctionSaleListingID(anyString())).thenReturn(new AuctionSaleListing("auctionSaleListingID", "productName", "productDescription", 0d, 0d, "currentHighestBidUserMail", "category", "sellerEmail", new GregorianCalendar(2024, Calendar.MARCH, 13, 12, 44).getTime(), true, true, true, "approverEmail", "messageFromApprover", false, new GregorianCalendar(2024, Calendar.MARCH, 13, 12, 44).getTime(), new GregorianCalendar(2024, Calendar.MARCH, 13, 12, 44).getTime(), new GregorianCalendar(2024, Calendar.MARCH, 13, 12, 44).getTime()));
-        when(jwtService.extractUserNameFromRequestHeaders(anyString())).thenReturn("sellerEmail");
-        when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(new User(1, "firstName", "lastName", "email", "password", RoleEnum.USER, 4.0, 5.0)));
+  User user;
 
-        String result = biddingServiceImpl.placeBid("authorizationHeader", "rand_uuid", 1.0);
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+    auctionProduct =
+        new AuctionSaleListing(
+            "auctionSaleListingID",
+            "productName",
+            "productDescription",
+            100d,
+            100d,
+            "currentHighestBidUserMail",
+            "category",
+            "sellerEmail",
+            new GregorianCalendar(2024, Calendar.MARCH, 13, 12, 44).getTime(),
+            true,
+            true,
+            true,
+            "approverEmail",
+            "messageFromApprover",
+            false,
+            new GregorianCalendar(2024, Calendar.MARCH, 13, 12, 44).getTime(),
+            new GregorianCalendar(2024, Calendar.MARCH, 13, 12, 44).getTime(),
+            new GregorianCalendar(2024, Calendar.MARCH, 13, 12, 44).getTime());
+    userBid = new Bidding(1, "rand_uuid", "test@testmail.com", 120.0);
+    user = new User(1, "firstName", "lastName", "email", "password", RoleEnum.USER, 4.0, 5.0);
+  }
 
-        Assertions.assertEquals(InfoConstant.BID_PLACED_SUCCESFULLY, result);
-    }
+  @Test
+  void testPlaceBid() {
+
+    when(auctionSaleListingRepository.findByAuctionSaleListingID(anyString()))
+        .thenReturn(auctionProduct);
+    when(auctionSaleListingRepository.save(any())).thenReturn(auctionProduct);
+    when(jwtService.extractUserNameFromRequestHeaders(anyString())).thenReturn("sellerEmail");
+    when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(user));
+    when(biddingRepository.save(any())).thenReturn(userBid);
+
+    String result = biddingServiceImpl.placeBid("authorizationHeader", "rand_uuid", 120.0);
+
+    Assertions.assertEquals(InfoConstant.BID_PLACED_SUCCESFULLY, result);
+  }
+
+  @Test
+  void testPlaceBidWhenUserNotFound() {
+
+    when(auctionSaleListingRepository.findByAuctionSaleListingID(anyString()))
+        .thenReturn(auctionProduct);
+    when(auctionSaleListingRepository.save(any())).thenReturn(auctionProduct);
+    when(jwtService.extractUserNameFromRequestHeaders(anyString())).thenReturn("sellerEmail");
+    when(userRepository.findByEmail(anyString())).thenReturn(Optional.empty());
+    when(biddingRepository.save(any())).thenReturn(userBid);
+
+    Assertions.assertThrows(
+        UsernameNotFoundException.class,
+        () -> biddingServiceImpl.placeBid("authorizationHeader", "rand_uuid", 1.0));
+  }
+
+  @Test
+  void testPlaceBidWhenProductNotFound() {
+    when(auctionSaleListingRepository.findByAuctionSaleListingID(anyString())).thenReturn(null);
+    when(auctionSaleListingRepository.save(any())).thenReturn(auctionProduct);
+    when(jwtService.extractUserNameFromRequestHeaders(anyString())).thenReturn("sellerEmail");
+    when(userRepository.findByEmail(anyString())).thenReturn(Optional.ofNullable(user));
+    when(biddingRepository.save(any())).thenReturn(userBid);
+
+    Assertions.assertThrows(
+        ListingNotFoundException.class,
+        () -> biddingServiceImpl.placeBid("authorizationHeader", "rand_uuid", 1.0));
+  }
+
+  @Test
+  void testPlaceBidWhenUserBidLessThanCurrentBid() {
+    when(auctionSaleListingRepository.findByAuctionSaleListingID(anyString()))
+        .thenReturn(auctionProduct);
+    when(auctionSaleListingRepository.save(any())).thenReturn(auctionProduct);
+    when(jwtService.extractUserNameFromRequestHeaders(anyString())).thenReturn("sellerEmail");
+    when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(user));
+    when(biddingRepository.save(any())).thenReturn(userBid);
+
+    Assertions.assertThrows(
+        LowBidException.class,
+        () -> biddingServiceImpl.placeBid("authorizationHeader", "rand_uuid", 100.0));
+  }
 }
