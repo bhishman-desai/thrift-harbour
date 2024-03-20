326e18d0e80264e0c6657956004efab12d344f29
Jainish Patel <jn891368@dal.ca>
2024-03-20 15:13:02 -0300

    feat: Get all auction sale list.

--- backend/thrift-harbour/src/main/java/tech/group15/thriftharbour/repository/AuctionSaleListingRepository.java
+++ backend/thrift-harbour/src/main/java/tech/group15/thriftharbour/repository/AuctionSaleListingRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import tech.group15.thriftharbour.model.AuctionSaleListing;
 import tech.group15.thriftharbour.model.ImmediateSaleListing;
 
+import java.util.Date;
 import java.util.List;
 
 public interface AuctionSaleListingRepository extends JpaRepository<AuctionSaleListing, String> {
@@ -22,4 +23,6 @@ public interface AuctionSaleListingRepository extends JpaRepository<AuctionSaleL
     @Query("SELECT i FROM AuctionSaleListing i WHERE i.auctionSaleListingID = ?1 and i.active = true and i.isApproved = true and i.isRejected = false and i.isSold = false")
     AuctionSaleListing findAuctionSaleProductByID(String auctionSaleListingID);
 
+    @Query("SELECT i FROM AuctionSaleListing i WHERE i.sellerEmail != ?1 and CAST(auctionSlot AS date) = ?2 and i.active = true and i.isApproved = true and i.isRejected = false")
+    List<AuctionSaleListing> findAllAuctionSaleListing(String sellerEmail, Date auctionSlot);
 }
