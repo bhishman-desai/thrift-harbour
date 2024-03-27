8400ee396c3c66e59e3089d384ccad8d141d9d4e
Jainish Patel <jn891368@dal.ca>
2024-03-27 16:06:44 -0300

    Test: Integration test for seller ratings.

--- backend/thrift-harbour/src/test/java/tech/group15/thriftharbour/controller/UserControllerTest.java
+++ backend/thrift-harbour/src/test/java/tech/group15/thriftharbour/controller/UserControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import tech.group15.thriftharbour.dto.request.BuyerRatingsRequest;
+import tech.group15.thriftharbour.dto.request.SellerRatingsRequest;
 import tech.group15.thriftharbour.dto.request.SignInRequest;
 import tech.group15.thriftharbour.dto.request.SignUpRequest;
 import tech.group15.thriftharbour.dto.response.GetListingImageResponse;
@@ -83,5 +84,51 @@ public class UserControllerTest {
         assertEquals(HttpStatus.OK, response.getStatusCode());
 
         this.jdbcTemplate.execute("DELETE FROM buyer_ratings WHERE rating_to_user_id = 1000");
+    }
+
+    @Test
+    public void testAddSellerRatings(){
+        SignUpRequest request = new SignUpRequest();
+        request.setEmail("mock@dal.ca");
+        request.setFirstName("Mock");
+        request.setLastName("Test");
+        request.setPassword("Mock@Test.com");
+
+        restTemplate.postForEntity("/api/v1/auth/signup", request, Void.class);
+
+        SignInRequest signInRequest = new SignInRequest();
+        signInRequest.setEmail("mock@dal.ca");
+        signInRequest.setPassword("Mock@Test.com");
+
+        ResponseEntity<SignInResponse> msg = restTemplate.postForEntity("/api/v1/auth/signin", signInRequest, SignInResponse.class);
+
+        String token = msg.getBody().getToken();
+        System.out.println("Token set" + token);
+
+        SellerRatingsRequest sellerRatingsRequest = new SellerRatingsRequest();
+        sellerRatingsRequest.setRatingToUserId(1000);
+        sellerRatingsRequest.setSellerRatings(5);
+
+        // Set the authentication token in the request headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", "Bearer " + token);
+
+        System.out.println(token);
+        // Create the request entity with headers
+        HttpEntity<SellerRatingsRequest> requestEntity = new HttpEntity<>(sellerRatingsRequest, headers);
+
+        this.jdbcTemplate.execute("insert into seller_ratings (rating_to_user_id, rating_from_user_id, seller_ratings) values (1000, 2, 5)");
+
+        // Send a Post request to the endpoint
+        ResponseEntity<String> response = restTemplate.exchange(
+                "http://localhost:" + port + "/api/v1/user/add-seller-ratings",
+                HttpMethod.POST,
+                requestEntity,
+                String.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        this.jdbcTemplate.execute("DELETE FROM seller_ratings WHERE rating_to_user_id = 1000");
     }
 }
